8b0137e60048d0b4d2eb59fe49e19aa0b0110621
SmallBlueWhale <349415101@qq.com>
2017-09-30 17:02:14 +0800

    1.外圈和三角形的动画弹出来先

--- app/src/main/java/com/hyx/whale/facedetectanimation/FaceDetectView/FaceDetectView.java
+++ app/src/main/java/com/hyx/whale/facedetectanimation/FaceDetectView/FaceDetectView.java
@@ -1,6 +1,8 @@
 package com.hyx.whale.facedetectanimation.FaceDetectView;
 
-import android.animation.ValueAnimator;
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -18,6 +20,8 @@ import android.view.animation.LinearInterpolator;
 
 import com.hyx.whale.facedetectanimation.R;
 
+import java.util.ArrayList;
+
 
 /**
  * funtion :
@@ -34,8 +38,9 @@ import com.hyx.whale.facedetectanimation.R;
 * 3.整体变小，并且颜色变成白色，然后开始显示白色
 * 4.逆时针转动
 * */
-public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callback {
-    private float radiu = 100;                                  //圆的半径
+public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callback, Animator.AnimatorListener {
+    private float radiu;                                  //圆的半径
+    private static final float CIRCLERADIU = 200;                                  //圆的半径
     private Point circleCenterPoint;                            //圆心位置
     private Point rectanglePoint1, rectanglePoint2;             //三角形的中心点位置
     private Point tanglePoint1, tanglePoint2, tanglePoint3;
@@ -54,13 +59,21 @@ public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callbac
     private Paint rectanglePaint;       //两个三角形的画笔
     private RectF rectF;                  //外接圆矩形
     private int rectLeft, rectTop, rectRight, rectBottom;
-    private FaceDetectThread faceDetectThread;
 
     private Path rectanglePath1;
     private Path rectanglePath2;
 
 
-    private ValueAnimator valueAnimator;
+    private ObjectAnimator bcRotateAnimation;
+
+    private AnimatorSet bcAnimatorSet;
+    private AnimatorSet bcAnimatorSet1;
+    private ObjectAnimator bcScaleAnimator1;
+    private ObjectAnimator bcScaleAnimator2;
+    private ObjectAnimator bcScaleAnimator3;
+    private ObjectAnimator bcScaleAnimator4;
+
+
     private SurfaceHolder mSurfaceHolder;   //绘图的canvas
     private Canvas canvas;
     private boolean isAnimationEnd;
@@ -76,8 +89,8 @@ public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callbac
     public FaceDetectView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initPaint();
-        initData();
         initView();
+        initData();
     }
 
 
@@ -151,7 +164,8 @@ public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callbac
     }
 
     private void initData() {
-        faceDetectThread = new FaceDetectThread();
+        radiu = CIRCLERADIU;
+        rectF = new RectF();
         rectanglePath1 = new Path();
         rectanglePath2 = new Path();
         circleCenterPoint = new Point();
@@ -163,22 +177,69 @@ public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callbac
         tanglePoint4 = new Point();
         tanglePoint5 = new Point();
         tanglePoint6 = new Point();
-        radiu = 200;
         circleCenterPoint.x = (int) radiu * 3 / 2;
         circleCenterPoint.y = (int) radiu * 3 / 2;
-        rectF = new RectF(-radiu + circleCenterPoint.x, -radiu + circleCenterPoint.y, radiu + circleCenterPoint.x, radiu + circleCenterPoint.y);
 
         initArcData();
 
-        valueAnimator = ValueAnimator.ofInt(0, 360);
-        valueAnimator.setDuration(1000);
-        valueAnimator.setInterpolator(new LinearInterpolator());
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                startAngle = (int) valueAnimator.getAnimatedValue();
-            }
-        });
+
+        if (null == bcScaleAnimator1) {
+            bcScaleAnimator1 = ObjectAnimator.ofFloat(this, "radiu", 0f, 1.04f);
+            bcScaleAnimator1.setDuration(200);
+            bcScaleAnimator1.setInterpolator(new LinearInterpolator());
+        }
+
+        if (null == bcScaleAnimator2) {
+            bcScaleAnimator2 = ObjectAnimator.ofFloat(this, "radiu", 1.04f, 1f);
+            bcScaleAnimator2.setDuration(66);
+            bcScaleAnimator2.setInterpolator(new LinearInterpolator());
+        }
+
+        if (null == bcScaleAnimator3) {
+            bcScaleAnimator3 = ObjectAnimator.ofFloat(this, "radiu", 1f, 1.02f);
+            bcScaleAnimator3.setDuration(66);
+            bcScaleAnimator3.setInterpolator(new LinearInterpolator());
+        }
+
+        if (null == bcScaleAnimator4) {
+            bcScaleAnimator4 = ObjectAnimator.ofFloat(this, "radiu", 1.02f, 1f);
+            bcScaleAnimator4.setDuration(66);
+            bcScaleAnimator4.setInterpolator(new LinearInterpolator());
+        }
+        if(null == bcAnimatorSet){
+            bcAnimatorSet = new AnimatorSet();
+            ArrayList<Animator> animatorArrayList = new ArrayList<Animator>();
+            animatorArrayList.add(bcScaleAnimator1);
+            animatorArrayList.add(bcScaleAnimator2);
+            animatorArrayList.add(bcScaleAnimator3);
+            animatorArrayList.add(bcScaleAnimator4);
+            bcAnimatorSet.playSequentially(animatorArrayList);
+        }
+
+        if (null == bcRotateAnimation) {
+            bcRotateAnimation = ObjectAnimator.ofInt(this, "startAngle", 0, 360);
+            bcRotateAnimation.setDuration(866);
+            bcRotateAnimation.setInterpolator(new LinearInterpolator());
+        }
+
+        if(null == bcAnimatorSet1){
+            bcAnimatorSet1 = new AnimatorSet();
+            ArrayList<Animator> animatorArrayList = new ArrayList<Animator>();
+            animatorArrayList.add(bcRotateAnimation);
+            bcAnimatorSet1.playSequentially(animatorArrayList);
+            bcAnimatorSet1.setStartDelay(800);
+        }
+    }
+
+
+    public void setStartAngle(int startAngle) {
+        this.startAngle = startAngle;
+        draw();
+    }
+
+    public void setRadiu(float radiu) {
+        this.radiu = CIRCLERADIU * radiu;
+        draw();
     }
 
     private void initArcData() {
@@ -188,6 +249,7 @@ public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callbac
         tanglePoint1 = caculateRectanglePosition(rectanglePoint1, startAngle + 180, 50);
         tanglePoint2 = caculateRectanglePosition(rectanglePoint1, startAngle + 120 + 180, 50);
         tanglePoint3 = caculateRectanglePosition(rectanglePoint1, startAngle + 240 + 180, 50);
+        rectanglePath1.reset();
         rectanglePath1.moveTo(tanglePoint1.x, tanglePoint1.y);
         rectanglePath1.lineTo(tanglePoint2.x, tanglePoint2.y);
         rectanglePath1.lineTo(tanglePoint3.x, tanglePoint3.y);
@@ -196,19 +258,21 @@ public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callbac
         tanglePoint4 = caculateRectanglePosition(rectanglePoint2, startAngle, 50);
         tanglePoint5 = caculateRectanglePosition(rectanglePoint2, startAngle + 120, 50);
         tanglePoint6 = caculateRectanglePosition(rectanglePoint2, startAngle + 240, 50);
+        rectanglePath2.reset();
         rectanglePath2.moveTo(tanglePoint4.x, tanglePoint4.y);
         rectanglePath2.lineTo(tanglePoint5.x, tanglePoint5.y);
         rectanglePath2.lineTo(tanglePoint6.x, tanglePoint6.y);
         rectanglePath2.close();
+
+        rectF.set(-radiu + circleCenterPoint.x, -radiu + circleCenterPoint.y, radiu + circleCenterPoint.x, radiu + circleCenterPoint.y);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        if (faceDetectThread == null) {
-            faceDetectThread = new FaceDetectThread();
+        if (bcAnimatorSet != null && bcAnimatorSet1 != null) {
+            bcAnimatorSet.start();
+            bcAnimatorSet1.start();
         }
-//        valueAnimator.start();
-        faceDetectThread.start();
     }
 
     @Override
@@ -218,37 +282,20 @@ public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callbac
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        if (faceDetectThread != null) {
-            faceDetectThread.isRunning = false;
-            faceDetectThread = null;
+        if (bcRotateAnimation != null) {
+            bcRotateAnimation.end();
         }
     }
 
-
-    private class FaceDetectThread extends Thread {
-        private boolean isRunning = true;
-
-        @Override
-        public void run() {
-            super.run();
-            while (isRunning) {
-                draw();
-                /* 动画结束之后主动结束这个动画，这样性能会好点*/
-                if (isAnimationEnd) {
-                    faceDetectThread = null;
-                    isRunning = false;
-                }
-            }
-        }
-    }
 
     private void draw() {
         try {
             synchronized (mSurfaceHolder) {
                 canvas = mSurfaceHolder.lockCanvas();
-                if(canvas!=null){
-                    //清屏操作
+                if (canvas != null) {
                     canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+                    Log.e("TAG" , "radiu:" + radiu);
+                    Log.e("TAG" , "startAngle:" + startAngle);
                     initArcData();
                     drawArc(canvas);
                 }
@@ -268,8 +315,8 @@ public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callbac
         canvas.drawArc(rectF, startAngle + 160 + 30, 160, false, circlePaint);
         canvas.drawPoint(rectanglePoint1.x, rectanglePoint1.y, rectanglePaint);
         canvas.drawPoint(rectanglePoint2.x, rectanglePoint2.y, rectanglePaint);
-        canvas.drawPath(rectanglePath1, circlePaint);
-        canvas.drawPath(rectanglePath2, circlePaint);
+        canvas.drawPath(rectanglePath1, rectanglePaint);
+        canvas.drawPath(rectanglePath2, rectanglePaint);
         Log.e("drawArc", "drawArc: " + startAngle);
     }
 
@@ -285,4 +332,33 @@ public class FaceDetectView extends SurfaceView implements SurfaceHolder.Callbac
     }
 
 
+    @Override
+    public void onAnimationStart(Animator animation, boolean isReverse) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation, boolean isReverse) {
+
+    }
+
+    @Override
+    public void onAnimationStart(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animator) {
+
+    }
 }
